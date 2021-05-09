be6deaaa884e86ae44e9377ec527cf73d71a3930
jaechung77 <jaechung77@gmail.com>
2021-05-09 20:23:27 +1000

    object handling

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyEmployee = Object.assign({}, employee);
+    delete copyEmployee[key];
+    return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
